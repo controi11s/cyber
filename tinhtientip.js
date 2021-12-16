874b127d45aaf88b7032f8aaabdbf2e2bf07ca92
Dat le <datlevan93@gmail.com>
2021-12-16 14:03:21 +0700

    Thu1

---
+++ tinhtientip.js
@@ -0,0 +1,10 @@
+var inputTongTienEl = document.getElementById("inputSoTien")
+
+var inputTongSoNguoiEL = document.getElementById("inputSonguoi")
+var SelectPhanTramEL = document.getElementById("PhanTram")
+
+function TinhTien(){
+    var inputTongTienEl=document.getElementById("inputSonguoi").value
+    var inputTongSoNguoiEL = document.getElementById("inputSonguoi").value
+    var SelectPhanTramEL = document.getElementById("PhanTram").value
+}
